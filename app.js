19410f6bfa80166489e34a3d509c5fa8b2ee6c92
Math33t3 <math33t3@stud.kea.dk>
2023-03-01 17:04:49 +0100

    created some countdown to node.js sessionh

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-    res.sendFile("/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(5000, () => {
