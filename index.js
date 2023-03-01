dff955334cbe1ed184e8987d9f53a6953515277c
Math33t3 <math33t3@stud.kea.dk>
2023-03-01 13:30:53 +0100

    created some new date time2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Express on Vercel");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(5000, () => {
